59bdefe84eb70182def2c28164dfc46b6a034a74
Nicol Gustin <nicol930519hotmail.com>
2023-05-26 19:21:54 -0500

    AutenticacionMedico

---
+++ Pages/Medico/Medico.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ciudadano_Sano.Pages.Medico
+{
+    public class MedicoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
