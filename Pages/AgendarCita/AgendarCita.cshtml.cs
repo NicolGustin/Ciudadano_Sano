d9724a56551905e17b39dc698ed72415dbbc6144
Nicol Gustin <nicol930519hotmail.com>
2023-05-26 19:30:28 -0500

    AutenticacionAgenCita

---
+++ Pages/AgendarCita/AgendarCita.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ciudadano_Sano.Pages.AgendarCita
+{
+    public class AgendarCitaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
