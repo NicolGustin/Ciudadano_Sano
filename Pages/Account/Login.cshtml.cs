a3830a48825a41da442e91e344f672e30c8ce9b4
Nicol Gustin <nicol930519hotmail.com>
2023-05-26 20:43:15 -0500

    Autenticacion

---
+++ Pages/Account/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ciudadano_Sano.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
